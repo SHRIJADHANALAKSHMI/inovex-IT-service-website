a26076574b1def3c3aa3f590dae33f883dd8590b
shrija <shrijasm@gmail.com>
2026-07-21 09:31:53 +0530

    Add contact schema fields

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-
+//name schema has been constructed
   name: {
     type: String,
     required: true,
   },
-
+//email schema has been constructed
   email: {
     type: String,
     required: true,
   },
-
+//message schema has been constructed
   message: {
     type: String,
     required: true,
